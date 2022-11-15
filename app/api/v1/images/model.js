460a9862f3386d10721bc3ba22c0aa66a4273454
dafasr <dafashanrizki2200@gmail.com>
2022-11-15 11:06:06 +0700

    -

--- app/api/v1/images/model.js
+++ app/api/v1/images/model.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 const { model, Schema } = mongoose;
 
-let imageSchema = Schema(
+let talentSchema = Schema(
   {
-    name: { type: String },
+    name: { type: String, required: [true, 'Nama  harus diisi'] },
+    role: { type: String, default: '-' },
+    image: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Image',
+      required: true,
+    },
   },
   { timestamps: true }
 );
 
-module.exports = model('Image', imageSchema);
+module.exports = model('Talent', talentSchema);
